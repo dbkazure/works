3aaf2c300c6e54b69d215c360f6b55ce9c79fd9f
dbkazure <129518376+dbkazure@users.noreply.github.com>
2023-11-01 21:34:01 +0530

    Add files via upload

---
+++ Powershell/103_DNS_record_check.ps1
@@ -0,0 +1,19 @@
+﻿# This powershell script will check the DNS records for each domain in the hostlist and update output in a text.
+# Please remember to update the paths for below variables.
+$hostlist = "D:\scripts\dnsrecordcheck\domains.txt"
+$outfile = "D:\scripts\DNSresults.txt"
+
+#DNS records to check - Enter it as array elements
+$records=@("A", "MX", "NS")
+
+"##########################$(date)######################################`n" >> $outfile
+foreach ($host_name in get-content "$hostlist")
+{
+">>>>>>>>>>>>>>>>>>>>>>>>>>>$host_name<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" >> $outfile
+foreach ($record in $records)
+{
+
+$lookupresult = Resolve-DnsName -Name $host_name -Type $record | Format-Table -Autosize
+$lookupresult >> $outfile
+}
+}

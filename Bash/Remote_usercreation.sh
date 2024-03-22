363c32227bc25980cff3e11730d05e5b08685def
dbkazure <129518376+dbkazure@users.noreply.github.com>
2024-03-22 15:59:28 +0530

    Update and rename Configure_Ansible_Client.sh to Remote_usercreation.sh

--- Bash/Remote_usercreation.sh
+++ Bash/Remote_usercreation.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-# This script will create a remote user and configure passwordless for it.
-# By running this as Ansible admin user, you can configure the remote machine as Ansible client.
-# After running the script, all you have to do for the Ansible client configuration is add the remote machine IP/hostname to the Ansible inventory.
+# This script will create user in remote computers
 
 read -p "Enter a username:" username
 read -sp "Enter a password:" password
 
 pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
 
-[[ ! -f ~/.ssh/id_rsa ]] && [[ ! -f ~/.ssh/id_rsa.pub ]] && ssh-keygen -t rsa -b 4096 -C "Ansbile admin user"
+[[ ! -f ~/.ssh/id_rsa ]] && [[ ! -f ~/.ssh/id_rsa.pub ]] && ssh-keygen -t rsa -b 4096 -C "Remote user"
 
 
 read -p $'\n'"Enter a remote machine which need to be configured as ansible client(adminuser@machine_ip;ex: admin@10.1.1.5):" credentials
 
-ip=$(echo "$credentials" | cut -d@ -f2)
+#ip=$(echo "$credentials" | cut -d@ -f2)
 ssh $credentials "sudo useradd -G sudo -p "$pass" -d "/home/$username" -m $username"
 
 if [[ ! $credentials = *'@'* ]]; then

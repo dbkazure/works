a2995e414ba588b7d7dc99daba7de0a0612719a9
dbkazure <129518376+dbkazure@users.noreply.github.com>
2024-03-23 20:29:09 +0530

    Update ansible_client_config.sh

--- Bash/ansible_client_config.sh
+++ Bash/ansible_client_config.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
-# This script will create a remote user and configure passwordless access using ssh keys.
-# Password-less user access on a remote server allows Ansible to seemlessly connect and execute commands/tasks on remote machine.
-# This script would add the user to the sudo group (i.e., sudo for Debian based and wheel for Redhat based) for elevated permission.
-# If you have a specific admin group, please set it on 'sudogroup' variable. 
+# This script will create a remote user and configure passwordless for it.
+# By running this as Ansible admin user, you can configure the remote machine as Ansible client.
 # After running the script, all you have to do for the Ansible client configuration is add the remote machine IP/hostname to the Ansible inventory.
 
-read -p "Enter a username:" username
+read -p "Enter ansible username:" username
 
-[[ ! -d /etc/ansible/userkeysi ]] && sudo mkdir /etc/ansible/userkeys
+if [ ! -d ~/.ssh ]; then
+        sudo mkdir ~/.ssh
+fi
 
-[[ ! -f /etc/ansible/userkeys/id_rsa ]] && [[ ! -f /etc/ansible/userkeys/id_rsa.pub ]] && sudo ssh-keygen -f /etc/ansible/userkeys/id_rsa -t rsa -b 4096 -C "Ansbile admin user"
+if [ ! -e ~/.ssh/id_rsa.pub ] || [ ! -e ~/.ssh/id_rsa ]; then
+        echo -e "\n Just taking a backup of the ".ssh" directory so as not to mess around with any private key within"
+        cp -R ~/.ssh ~/.ssh-`date +%Y%b%d`
+        echo -e "\nKeys doesn't exists.. Generating new... \n"
+        sudo ssh-keygen -f ~/.ssh/id_rsa -t rsa -b 4096 -C "Ansbile admin user"
 
 #pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
 
-#[[ ! -f ~/.ssh/id_rsa ]] && [[ ! -f ~/.ssh/id_rsa.pub ]] && ssh-keygen -t rsa -b 4096 -C "Ansbile admin user"
-
-# Collect remote login details..
-read -p "Enter Login details of a remote machine which need to be configured as ansible client(adminuser@machine_ip;ex: admin@10.1.1.5):" credentials
+# Collect remote admin or root login details..
+read -p "Enter remote admin login details(adminuser@machine_ip;ex: admin@10.1.1.5):" credentials
 if [[ ! $credentials = *'@'* ]]; then
  echo -e "\n Incorrect remote details...\n Please startover and enter the remote machine details in the correct format: adminuser@remoteip or adminuser@remotehostname"
  exit
@@ -33,14 +35,14 @@ echo "Sudo group is: $sudogroup"
 
 #ip=$(echo "$credentials" | cut -d@ -f2)
 ssh $credentials "sudo useradd -G '$sudogroup' -d '/home/$username' -m $username"
-sudo scp /etc/ansible/userkeys/id_rsa.pub $credentials:/tmp/
+sudo scp ~/.ssh/id_rsa.pub $credentials:/tmp/
 ssh $credentials "sudo mkdir /home/$username/.ssh && sudo chown $username:$username /home/$username/.ssh; \
         sudo cat /tmp/id_rsa.pub | sudo tee -a /home/$username/.ssh/authorized_keys && sudo chmod 600 /home/$username/.ssh/authorized_keys; \
         sudo chown -R $username:$username /home/$username/.ssh/"
 
 if [ $? -ne 0 ];then
     echo "Looks like password authentication is not enabled on the remote machine, let try with ssh keys..."
-    read -p "Enter the path [/etc/ansible/userkeys/admin/id_rsa]:" key
+    read -p "Enter the remote admin private key file [/etc/ansible/userkeys/admin/id_rsa]:" key
     if [ $key == ""];then
             key="/etc/ansible/userkeys/admin/id_rsa"
     fi
@@ -52,7 +54,7 @@ if [ $? -ne 0 ];then
     fi
     echo "Sudo group is: $sudogroup"
     ssh -i $key $credentials "sudo useradd -G '$sudogroup' -d '/home/$username' -m $username"
-    sudo scp -i $key /etc/ansible/userkeys/id_rsa.pub $credentials:/tmp/
+    sudo scp -i $key ~/.ssh/id_rsa.pub $credentials:/tmp/
     ssh -i $key $credentials "sudo mkdir /home/$username/.ssh && sudo chown $username:$username /home/$username/.ssh; \
                 sudo cat /tmp/id_rsa.pub | sudo tee -a /home/$username/.ssh/authorized_keys && sudo chmod 700 /home/$username/.ssh/authorized_keys; \
                sudo chown -R $username:$username /home/$username/.ssh"
